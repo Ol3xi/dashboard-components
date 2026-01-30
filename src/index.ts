e6ed33ffdf695b883cb8f06629995e872508a64d
Ol3xi <olexitom29@gmail.com>
2026-01-30 01:21:42 +0100

    removed index.ts

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all components from a single entry point
-/* export { StatsCard } from './components/StatsCard';
-export type { 
-  StatsCardProps, 
-  ColorVariant, 
-  SizeVariant 
-} from './components/StatsCard'; */
